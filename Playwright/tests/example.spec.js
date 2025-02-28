f7c816de5bf76109a7aa039db41353b779a86aa2
k-suzumi <k-suzumi@CN24-R-0161.local>
2025-02-28 12:15:31 +0900

    ページ全体をスクショするように修正

--- Playwright/tests/example.spec.js
+++ Playwright/tests/example.spec.js
@@ -2,11 +2,9 @@
 import { test, expect } from '@playwright/test';
 
 test("本番環境と検証環境のスクリーンショット比較", async ({ page }, testInfo) => {
-  await page.goto("https://v2.spaia-keiba.com/races/20250301")
-  await page.waitForFunction(() => document.fonts.ready);
+  await page.goto("https://v2.spaia-keiba.com/races/20250301", { waitUntil: 'networkidle' })
   await page.screenshot({ path: `${testInfo.snapshotPath("result.png")}`, fullPage: true })
 
-  await page.goto("https://staging-beta.spaia-keiba.com/races/20250301")
-  await page.waitForFunction(() => document.fonts.ready);
+  await page.goto("https://staging-beta.spaia-keiba.com/races/20250301", { waitUntil: 'networkidle' });
   expect(await page.screenshot({ fullPage: true })).toMatchSnapshot({ name: "result.png" })
 })
